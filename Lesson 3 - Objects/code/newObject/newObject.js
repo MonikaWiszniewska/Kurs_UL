f214a2a4f7c2d5efd41fad5001c368de7f3dd6a6
Monika Wiszniewska <nikawisz@gmail.com>
2016-03-06 14:10:10 +0100

    test

--- Lesson 3 - Objects/code/newObject/newObject.js
+++ Lesson 3 - Objects/code/newObject/newObject.js
@@ -3,14 +3,22 @@
 		global.UAM = {};
 	}
 
-	function newObject() {}
+	function newObject(constr, arg1, arg2) {
+       return  constr = function(arg1, arg2){
+            this.arg1 = arg1;
+            this.arg2 = arg2;
+        }
+    }
 
 	global.UAM.newObject = newObject;
+   
 }(window));
 
 /*
-	Zaimplementuj funkcję newObject, która będzie działać analogicznie do operatora new. Pierwszym parametrem funkcji niech będzie
-	konstruktor, natomiast pozostałe to parametry konstruktora. Przykładowe zastosowanie:
+	Zaimplementuj funkcję newObject, która będzie działać analogicznie 
+    do operatora new. Pierwszym parametrem funkcji niech będzie
+	konstruktor, natomiast pozostałe to parametry konstruktora.
+    Przykładowe zastosowanie:
 
 	new MyClass(arg1, arg2) -> newObject(MyClass, arg1, arg2)
 */
