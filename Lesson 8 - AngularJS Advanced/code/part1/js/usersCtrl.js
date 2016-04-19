fb295c9748c19119ae5001e3670ec50f56bc7cb4
Monika Wiszniewska <nikawisz@gmail.com>
2016-04-19 17:03:31 +0200

    19kwietnia

--- Lesson 8 - AngularJS Advanced/code/part1/js/usersCtrl.js
+++ Lesson 8 - AngularJS Advanced/code/part1/js/usersCtrl.js
@@ -1,3 +1,9 @@
 angular.module('myapp').controller('UsersCtrl', function ($scope, usersList) {
-
+   $scope.List = usersList;
 });
+
+
+//wejdz do katalogu part1 i
+// npm install 
+// a potem node server.js 
+// zeby uruchomic server
